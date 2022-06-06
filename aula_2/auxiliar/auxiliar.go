7ca43acfe31eaccf7e82c84df9b0803bf245d348
felixtitonina <felixtitonina@outlook.com>
2022-06-06 00:33:55 -0300

    first commit

---
+++ aula_2/auxiliar/auxiliar.go
@@ -0,0 +1,11 @@
+package auxiliar
+
+import "fmt"
+
+/**
+Uma função "publica" deve começar como letra maiuscula ai ela podera ser exportada por outros pacotes
+minusculo somente no pacote que ela esta
+*/
+func Escrever() {
+	fmt.Printf("Escrevendo do pacote auxiliar")
+}
